917ff76f429880457b6d538e0e2a7badcc6b703b
Eleven <idiotalex@163.com>
2019-05-13 16:13:17 +0800

    add annotation for sheet

---
+++ src/main/java/com.hotstrip/annotation/Sheet.java
@@ -0,0 +1,19 @@
+package com.hotstrip.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 单个 sheet 表的元注解
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Sheet {
+    // 字段名称
+    String name();
+
+    // 宽度
+    int width() default 15;
+}
