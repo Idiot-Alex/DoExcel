4fe2c41ae98f62c390a5103c34d6c4306fcc02ee
Eleven <idiotalex@163.com>
2019-08-08 15:45:17 +0800

    添加配置类

--- src/main/java/com.hotstrip/excel/DoExcel.java
+++ src/main/java/com.hotstrip/excel/DoExcel.java
@@ -4,4 +4,26 @@ package com.hotstrip.excel;
  * Created by idiot on 2019/7/28.
  */
 public class DoExcel {
+    // excelConfig
+    private ExcelConfig excelConfig;
+
+    public DoExcel() {
+    }
+
+    public ExcelConfig getExcelConfig() {
+        return excelConfig;
+    }
+
+    public void setExcelConfig(ExcelConfig excelConfig) {
+        this.excelConfig = excelConfig;
+    }
+
+    /**
+     * ExcelWriter
+     */
+    public ExcelWriter getExcelWriter() {
+        return ExcelWriter.builder()
+                .title("test.xlsx")
+                .build();
+    }
 }
