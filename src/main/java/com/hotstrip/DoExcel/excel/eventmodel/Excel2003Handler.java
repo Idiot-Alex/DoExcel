d769dad1768cbb6b94660d7c63e057dd51624bee
idiotalex@163.com <idiotalex@163.com>
2019-12-13 13:50:39 +0800

    添加代码 区分 Excel 2003 各种类型数据

--- src/main/java/com/hotstrip/DoExcel/excel/eventmodel/Excel2003Handler.java
+++ src/main/java/com/hotstrip/DoExcel/excel/eventmodel/Excel2003Handler.java
@@ -1,6 +1,9 @@
 package com.hotstrip.DoExcel.excel.eventmodel;
 
 import org.apache.poi.hssf.eventusermodel.*;
+import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
+import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingCellDummyRecord;
+import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingRowDummyRecord;
 import org.apache.poi.hssf.record.*;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.slf4j.Logger;
@@ -8,6 +11,8 @@ import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Administrator on 2019/7/29.
@@ -25,6 +30,12 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
     // 当前行号
     private int currentRowNum;
 
+    // SSTRecord
+    private SSTRecord sstRecord;
+
+    // 当前行数据集
+    private List<String> rowList = new ArrayList<String>();
+
     /**
      * 监听方法，处理 Record
      * 关于常用的 Record 的 sid 如下:
@@ -32,13 +43,10 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
      * BOFRecord: Workbook、Sheet 的开始
      * BlankRecord: 存在单元格样式的空单元格
      * BoolErrRecord: 布尔或错误单元格
-     * FormulaRecord: 公式单元格
      * StringRecord: 公式的计算结果单元格
-     * LabelRecord: 文本单元格
      * LabelSSTRecord: 共用的文本单元格
      * NumberRecord: 数值单元格：数字单元格和日期单元格
      * EOFRecord: Workbook、Sheet的结束
-     * RowRecord: 行记录
      * @param record
      */
     public void processRecord(Record record) {
@@ -54,27 +62,40 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
                     sheetIndex ++;
                 break;
             case BlankRecord.sid:
+                BlankRecord blankRecord = (BlankRecord) record;
+                logger.info("blankRecord: {}", "有样式，值为空");
                 break;
             case BoolErrRecord.sid:
-                break;
-            case FormulaRecord.sid:
+                BoolErrRecord boolErrRecord = (BoolErrRecord) record;
+                logger.info("boolErrRecord: errorValue: {} boolValue: {}", boolErrRecord.getErrorValue(), boolErrRecord.getBooleanValue());
+                logger.info("boolErrRecord: isError:{} isBool: {}", boolErrRecord.isError(), boolErrRecord.isBoolean());
                 break;
             case StringRecord.sid:
+                StringRecord stringRecord = (StringRecord) record;
+                logger.info("stringRecord: {}", stringRecord.getString());
                 break;
-            case LabelRecord.sid:
+            case SSTRecord.sid:
+                sstRecord = (SSTRecord) record;
                 break;
             case LabelSSTRecord.sid:
+                LabelSSTRecord labelSSTRecord = (LabelSSTRecord) record;
+                logger.info("labelSSTRecord: {}", sstRecord.getString(labelSSTRecord.getSSTIndex()));
                 break;
             case NumberRecord.sid:
-                break;
-            case EOFRecord.sid:
-                break;
-            case RowRecord.sid:
-                RowRecord rowRecord = (RowRecord) record;
-                logger.info("rowNumber: [{}]...firstCol: [{}]...lastCol: [{}]", rowRecord.getRowNumber(), rowRecord.getFirstCol(), rowRecord.getLastCol());
+                NumberRecord numberRecord = (NumberRecord) record;
+                logger.info("numberRecord: {}", numberRecord.getValue());
                 break;
             default:
                 break;
+        }
+        // 空值的操作
+        if (record instanceof MissingCellDummyRecord) {
+            logger.info("该列为空值");
+        }
+        // 如果是一行的最后一列
+        if (record instanceof LastCellOfRowDummyRecord) {
+            LastCellOfRowDummyRecord lastCellOfRowDummyRecord = (LastCellOfRowDummyRecord) record;
+            logger.info("lastCellOfRowDummyRecord: lastCol: {} 新行", lastCellOfRowDummyRecord.getLastColumnNumber());
         }
     }
 
