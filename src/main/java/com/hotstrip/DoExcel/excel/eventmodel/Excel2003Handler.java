ea941b9cb3b62e9b56e6d681549230fa4c8b6f3a
idiotalex@163.com <idiotalex@163.com>
2019-12-14 17:55:43 +0800

    更新代码 处理 Excel 2003 解析完成

--- src/main/java/com/hotstrip/DoExcel/excel/eventmodel/Excel2003Handler.java
+++ src/main/java/com/hotstrip/DoExcel/excel/eventmodel/Excel2003Handler.java
@@ -1,5 +1,6 @@
 package com.hotstrip.DoExcel.excel.eventmodel;
 
+import com.alibaba.fastjson.JSON;
 import org.apache.poi.hssf.eventusermodel.*;
 import org.apache.poi.hssf.eventusermodel.dummyrecord.LastCellOfRowDummyRecord;
 import org.apache.poi.hssf.eventusermodel.dummyrecord.MissingCellDummyRecord;
@@ -26,14 +27,18 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
     // 当前单元表下标
     private int sheetIndex = -1;
 
-    // 当前行号
+    // 当前行号 0 表示第一行
     private int currentRowNum;
+    // 当前列号 0 表示第一列
+    private int currentColNum;
 
     // SSTRecord
     private SSTRecord sstRecord;
 
     // 当前行数据集
     private List<String> rowList = new ArrayList<String>();
+
+
 
     /**
      * 监听方法，处理 Record
@@ -42,7 +47,8 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
      * BOFRecord: Workbook、Sheet 的开始
      * BlankRecord: 存在单元格样式的空单元格
      * BoolErrRecord: 布尔或错误单元格
-     * StringRecord: 公式的计算结果单元格
+     * FormulaRecord: 公式单元格 => 比如 =SUM(B1:B2)
+     * StringRecord: 公式的计算结果单元格 => 比如 =(B2)
      * LabelSSTRecord: 共用的文本单元格
      * NumberRecord: 数值单元格：数字单元格和日期单元格
      * EOFRecord: Workbook、Sheet的结束
@@ -52,50 +58,81 @@ public class Excel2003Handler implements HSSFListener, ExcelHandler {
         switch (record.getSid()){
             case BoundSheetRecord.sid:
                 BoundSheetRecord boundSheetRecord = (BoundSheetRecord) record;
-                logger.info("current process workSheet is: [{}]", boundSheetRecord.getSheetname());
+                logger.debug("current process workSheet is: [{}]", boundSheetRecord.getSheetname());
                 break;
             case BOFRecord.sid:
                 BOFRecord bofRecord = (BOFRecord) record;
                 // 如果类型是 单元表，当前单元表下标 +1
-                if (bofRecord.getType() == BOFRecord.TYPE_WORKSHEET)
-                    sheetIndex ++;
+                if (bofRecord.getType() == BOFRecord.TYPE_WORKSHEET){
+                    this.sheetIndex ++;
+                    // 当前行号重置为 0
+                    this.currentRowNum = 0;
+                }
                 break;
             case BlankRecord.sid:
-                BlankRecord blankRecord = (BlankRecord) record;
-                logger.info("blankRecord: {}", "有样式，值为空");
+                // BlankRecord blankRecord = (BlankRecord) record;
+                addFieldValue("");
                 break;
             case BoolErrRecord.sid:
                 BoolErrRecord boolErrRecord = (BoolErrRecord) record;
-                logger.info("boolErrRecord: errorValue: {} boolValue: {}", boolErrRecord.getErrorValue(), boolErrRecord.getBooleanValue());
-                logger.info("boolErrRecord: isError:{} isBool: {}", boolErrRecord.isError(), boolErrRecord.isBoolean());
+                String value = boolErrRecord.isBoolean() ? String.valueOf(boolErrRecord.getBooleanValue()) : String.valueOf(boolErrRecord.getErrorValue());
+                addFieldValue(value);
+                break;
+            case FormulaRecord.sid:
+                FormulaRecord formulaRecord = (FormulaRecord) record;
+                /**
+                 * 该判断用于区分 =(B3) 和 =SUM(B1:B4)
+                 * 换句话说，区分赋值类公式和函数类公式（并未做完整测试，可能不准确）
+                 * ----------------------------------------------------------------------
+                 * 该判断结果若为 true 就会触发 FormulaRecord.sid 和 StringRecord.sid 两种 sid
+                 * 但是实际上只有在 StringRecord.sid 里面才能取到真正的值
+                 */
+                if (!formulaRecord.hasCachedResultString()) {
+                    addFieldValue(String.valueOf(formulaRecord.getValue()));
+                }
                 break;
             case StringRecord.sid:
                 StringRecord stringRecord = (StringRecord) record;
-                logger.info("stringRecord: {}", stringRecord.getString());
+                addFieldValue(stringRecord.getString());
                 break;
+            // SSTRecord 保存 xls 文件中唯一的 String
             case SSTRecord.sid:
-                sstRecord = (SSTRecord) record;
+                this.sstRecord = (SSTRecord) record;
                 break;
+            // LabelSSTRecord 取出索引值，集合上面的 SSTRecord 去取值
             case LabelSSTRecord.sid:
                 LabelSSTRecord labelSSTRecord = (LabelSSTRecord) record;
-                logger.info("labelSSTRecord: {}", sstRecord.getString(labelSSTRecord.getSSTIndex()));
+                addFieldValue(sstRecord.getString(labelSSTRecord.getSSTIndex()).toString().trim());
                 break;
             case NumberRecord.sid:
                 NumberRecord numberRecord = (NumberRecord) record;
-                logger.info("numberRecord: {}", numberRecord.getValue());
+                addFieldValue(String.valueOf(numberRecord.getValue()));
                 break;
             default:
                 break;
         }
         // 空值的操作
         if (record instanceof MissingCellDummyRecord) {
-            logger.info("该列为空值");
+            // MissingCellDummyRecord missingCellDummyRecord = (MissingCellDummyRecord) record;
+            addFieldValue("");
         }
         // 如果是一行的最后一列
         if (record instanceof LastCellOfRowDummyRecord) {
-            LastCellOfRowDummyRecord lastCellOfRowDummyRecord = (LastCellOfRowDummyRecord) record;
-            logger.info("lastCellOfRowDummyRecord: lastCol: {} 新行", lastCellOfRowDummyRecord.getLastColumnNumber());
+            // LastCellOfRowDummyRecord lastCellOfRowDummyRecord = (LastCellOfRowDummyRecord) record;
+            logger.info(JSON.toJSONString(this.rowList));
+            this.rowList.clear();
+            // 行号自增
+            this.currentRowNum++;
+            // 列号设置为 0
+            this.currentColNum = 0;
         }
+    }
+
+    // 添加字段值 修改当前列号
+    private void addFieldValue(String value) {
+        this.rowList.add(this.currentColNum, value);
+        // 列号自增
+        this.currentColNum++;
     }
 
     /**
